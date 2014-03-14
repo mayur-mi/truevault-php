a1b9348b670982a88f47f7bdc0cac84b504261c3
Marek Vavrečan <vavrecan@gmail.com>
2014-03-14 13:00:04 +0100

    installation steps

--- TrueVault/TrueVault.php
+++ TrueVault/TrueVault.php
@@ -79,6 +79,7 @@ class TrueVault {
     public static $CURL_OPTS = array(
         CURLOPT_CONNECTTIMEOUT => 10,
         CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FRESH_CONNECT  => true,
         CURLOPT_TIMEOUT        => 60,
         CURLOPT_USERAGENT      => "truevault-php"
     );
